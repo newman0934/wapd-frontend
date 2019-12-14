7c3303a9959698848c13f86704f8a307c2d10ea0
Vivian Lo <yunyubee@gmail.com>
2019-12-14 18:58:23 +0800

    Add UI of product page with relevent components

--- src/components/productBreadcrumb.vue
+++ src/components/productBreadcrumb.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="border">
-    <h1>productBreadcrumb</h1>
+  <div>
+    <ul class="breadcrumb">
+      <li>
+        <a href="#">Home</a>
+        <span class="divider">></span>
+      </li>
+      <li>
+        <a href="#">store</a>
+        <span class="divider">></span>
+      </li>
+      <li>
+        <a href="#">category</a>
+        <span class="divider">></span>
+      </li>
+      <li class="active">I'm Product</li>
+    </ul>
   </div>
 </template>
--- src/components/productGallery.vue
+++ src/components/productGallery.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="border">
-    <h1>productGallery</h1>
+  <div>
+    <img
+      src="https://picsum.photos/id/1004/5616/3744"
+      class="img-fluid mt-3"
+      alt="Responsive image"
+    />
+    <img
+      src="https://picsum.photos/id/1012/3973/2639"
+      class="img-fluid mt-3"
+      alt="Responsive image"
+    />
   </div>
 </template>
