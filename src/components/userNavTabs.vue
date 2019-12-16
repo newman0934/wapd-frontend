20af76f325eeb26e0a91a9db14427ce98efdc4fb
Vivian Lo <yunyubee@gmail.com>
2019-12-16 12:21:04 +0800

    Add userNavTabs components

---
+++ src/components/userNavTabs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="card border-white w-100">
+      <div class="card-img user-bg">
+        <img src="https://picsum.photos/id/177/2515/1830" class="img-fluid" alt="user-bg" />
+      </div>
+      <div class="card-img-overlay mt-5">
+        <h4>Hi, user@example.com</h4>
+      </div>
+    </div>
+    <ul class="nav nav-tabs mt-2">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">訂單查詢</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">帳號管理</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">重設密碼</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Wish List</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+.user-bg {
+  height: 150px;
+  overflow: hidden;
+}
+
+.user-bg img {
+  margin-top: -5%;
+  opacity: 0.7;
+  filter: alpha(opacity=70);
+}
+</style>
--- src/views/userEdit.vue
+++ src/views/userEdit.vue
@@ -1,5 +1,13 @@
 <template>
-      <div class="container py-5">
-    <h1 class="mt-5">會員資料編輯頁</h1>
+  <div class="container py-5">
+    <userNavTabs />
   </div>
 </template>
+<script>
+import userNavTabs from "./../components/userNavTabs";
+export default {
+  components: {
+    userNavTabs
+  }
+};
+</script>
