bfbab680ae5ade100fd8046ec8e0e427e5e3aad4
Vivian Lo <yunyubee@gmail.com>
2019-12-19 22:01:23 +0800

    Add import usersAPI in userEdit

--- src/views/userEdit.vue
+++ src/views/userEdit.vue
@@ -1,35 +1,113 @@
 <template>
   <div class="container">
     <div class="d-flex justify-content-center">
-      <form class="w-75 mt-4">
+      <form class="w-75 mt-4" @submit.stop.prevent="handleSubmit">
         <div class="form-label-group mb-2">
-          <input id="name" name="Name" type="text" class="form-control" placeholder="name" />
+          <input
+            id="name"
+            name="name"
+            v-model="name"
+            type="text"
+            class="form-control"
+            placeholder="Enter name"
+          />
         </div>
         <div class="form-label-group mb-2">
           <input
             id="email"
             name="email"
+            v-model="email"
             type="email"
             class="form-control"
-            placeholder="user@example.com"
+            placeholder="email"
             disabled
           />
         </div>
-        <div class="form-label-group mb-2">
+        <!-- <div class="form-label-group mb-2">
           <input id="birth" name="birth" type="date" class="form-control" value="2019-12-25" />
-        </div>
+        </div>-->
         <div class="form-label-group mb-2">
-          <input id="address" name="address" type="text" class="form-control" placeholder="Address" />
+          <input
+            id="address"
+            name="address"
+            v-model="address"
+            type="text"
+            class="form-control"
+            placeholder="Enter address"
+          />
         </div>
         <div class="form-label-group mb-3">
-          <input id="phone" name="phone" type="text" class="form-control" placeholder="Phone" />
+          <input
+            id="phone"
+            name="phone"
+            v-model="phone"
+            type="text"
+            class="form-control"
+            placeholder="Enter phone number"
+          />
         </div>
 
-        <button class="btn btn-lg btn-outline-secondary btn-block btn-sm mb-3" type="submit">更新會員資料</button>
+        <button
+          class="btn btn-lg btn-outline-secondary btn-block btn-sm mb-3"
+          type="submit"
+          :disabled="isProcessing"
+        >{{isProcessing ? "資料更新中..." : "Submit"}}</button>
       </form>
     </div>
   </div>
 </template>
 <script>
-export default {};
+/* eslint-disable */
+import usersAPI from "./../apis/users";
+import { mapState } from "vuex";
+import { Toast } from "./../utils/helpers";
+export default {
+  data() {
+    return {
+      id: 0,
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+      isProcessing: false
+    };
+  },
+  computed: {
+    ...mapState(["currentUser"])
+  },
+  watch: {
+    currentUser(user) {
+      this.setUser();
+    }
+  },
+  created() {
+    const { id } = this.$route.params;
+    if (id.toString() !== this.currentUser.id.toString()) {
+      this.$router.push({ name: "notFound" });
+      return;
+    }
+    this.setUser();
+  },
+  methods: {
+    setUser() {
+      this.id = this.currentUser.id;
+      this.name = this.currentUser.name;
+      this.email = this.currentUser.email;
+      this.phone = this.currentUser.phone;
+      this.address = this.currentUser.address;
+    }
+    // async handleSubmit(e) {
+    //   if (this.email !== this.currentUser.email.toString()) {
+    //     Toast.fire({
+    //       type: "warning",
+    //       title: "更新錯誤"
+    //     });
+    //     return;
+    //   }
+    //   const form = e.target;
+    //   const formData = newFormData(form);
+
+    // }
+  }
+};
 </script>
