d9dbff661427f2dd01cce59ba218f5dba0ef714d
Vivian Lo <yunyubee@gmail.com>
2020-01-10 21:04:21 +0800

    fix: cart with no items

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -1,108 +1,119 @@
 <template>
   <div class="container py-5">
-    <h1>My Cart</h1>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">商品名稱</th>
-          <th scope="col">價格</th>
-          <th scope="col">
-            數量
-            <small class="text-muted">(雙擊修改數量)</small>
-          </th>
-          <th scope="col">小計</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in items" :key="item.id">
-          <th scope="row">
-            <router-link :to="{name:'product', params:{id:item.ProductId}}">
-              <vue-load-image>
-                <img
-                  slot="image"
-                  :src="item.images[0].url"
-                  class="img-fluid"
-                  alt="Responsive image"
-                  width="150px"
-                />
-                <img slot="preloader" class="img-fluid" width="150px" src="./../static/loading.gif" />
-                <div slot="error" class="img-fluid">圖片下載失敗</div>
-              </vue-load-image>
-            </router-link>
-          </th>
-          <td class="align-middle">
-            <router-link :to="{name:'product', params:{id:item.ProductId}}">{{item.name}}</router-link>
-            <p>Color:{{item.color}}, Size:{{item.size}}</p>
-          </td>
-          <td class="align-middle">{{item.sell_price | currency}}</td>
-          <td class="align-middle" @dblclick="editQty(item)">
-            <div v-if="item.id === cacheItem.id">
-              <form @submit.prevent.stop="putCartItem(item)">
-                <div>
-                  <input
-                    id="quantity"
-                    type="number"
-                    name="quantity"
-                    value="1"
-                    v-model="cacheQty"
-                    data-decimals="0"
-                    min="1"
-                    max="20"
-                    step="1"
+    <div v-if="items.length == 0">
+      <h1>My Cart</h1>
+      <h3 class="mt-4">您還沒加入商品到購物車喔，快去商場看看吧！</h3>
+    </div>
+    <div v-else>
+      <h1>My Cart</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">商品名稱</th>
+            <th scope="col">價格</th>
+            <th scope="col">
+              數量
+              <small class="text-muted">(雙擊修改數量)</small>
+            </th>
+            <th scope="col">小計</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.id">
+            <th scope="row">
+              <router-link :to="{name:'product', params:{id:item.ProductId}}">
+                <vue-load-image>
+                  <img
+                    slot="image"
+                    :src="item.images[0].url"
+                    class="img-fluid"
+                    alt="Responsive image"
+                    width="150px"
                   />
-                </div>
-                <button class="btn" type="submit">done</button>
-              </form>
-              <a @click.stop.prevent="cancelEdit()">cancel</a>
-            </div>
-            <div v-else>{{item.quantity}}</div>
-          </td>
-          <td class="align-middle">{{item.sell_price*item.quantity | currency}}</td>
-          <td class="align-middle">
-            <button
-              type="button"
-              class="btn btn-outline-danger"
-              @click.stop.prevent="deleteCartItem(item.id)"
-            >Ｘ</button>
-          </td>
-        </tr>
-        <tr v-if="coupon.isValid">
-          <th scope="row"></th>
-          <td colspan="3">折扣碼({{coupon.coupon_code}})</td>
-          <td>- {{coupon.discount_amount | currency}}</td>
-          <td></td>
-        </tr>
-        <tr>
-          <th scope="row"></th>
-          <td colspan="3">小計</td>
-          <td>{{total | currency}}</td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+                  <img
+                    slot="preloader"
+                    class="img-fluid"
+                    width="150px"
+                    src="./../static/loading.gif"
+                  />
+                  <div slot="error" class="img-fluid">圖片下載失敗</div>
+                </vue-load-image>
+              </router-link>
+            </th>
+            <td class="align-middle">
+              <router-link :to="{name:'product', params:{id:item.ProductId}}">{{item.name}}</router-link>
+              <p>Color:{{item.color}}, Size:{{item.size}}</p>
+            </td>
+            <td class="align-middle">{{item.sell_price | currency}}</td>
+            <td class="align-middle" @dblclick="editQty(item)">
+              <div v-if="item.id === cacheItem.id">
+                <form @submit.prevent.stop="putCartItem(item)">
+                  <div>
+                    <input
+                      id="quantity"
+                      type="number"
+                      name="quantity"
+                      value="1"
+                      v-model="cacheQty"
+                      data-decimals="0"
+                      min="1"
+                      max="20"
+                      step="1"
+                    />
+                  </div>
+                  <button class="btn" type="submit">done</button>
+                </form>
+                <a @click.stop.prevent="cancelEdit()">cancel</a>
+              </div>
+              <div v-else>{{item.quantity}}</div>
+            </td>
+            <td class="align-middle">{{item.sell_price*item.quantity | currency}}</td>
+            <td class="align-middle">
+              <button
+                type="button"
+                class="btn btn-outline-danger"
+                @click.stop.prevent="deleteCartItem(item.id)"
+              >Ｘ</button>
+            </td>
+          </tr>
+          <tr v-if="coupon.isValid">
+            <th scope="row"></th>
+            <td colspan="3">折扣碼({{coupon.coupon_code}})</td>
+            <td>- {{coupon.discount_amount | currency}}</td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row"></th>
+            <td colspan="3">小計</td>
+            <td>{{total | currency}}</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
 
-    <form class="form-inline ml-3">
-      <div class="form-group mx-sm-3">
-        <label for="input-coupon" class="sr-only">輸入優惠序號</label>
-        <input
-          type="text"
-          class="form-control"
-          v-model="couponCode"
-          id="couponCode"
-          name="couponCode"
-          placeholder="輸入優惠序號"
-        />
-      </div>
-      <button class="btn btn-outline-primary" @click.prevent.stop="postCoupon(couponCode)">使用</button>
-    </form>
+      <form class="form-inline ml-3">
+        <div class="form-group mx-sm-3">
+          <label for="input-coupon" class="sr-only">輸入優惠序號</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="couponCode"
+            id="couponCode"
+            name="couponCode"
+            placeholder="輸入優惠序號"
+          />
+        </div>
+        <button class="btn btn-outline-primary" @click.prevent.stop="postCoupon(couponCode)">使用</button>
+      </form>
 
-    <button
-      class="btn btn-outline-secondary btn-block mt-3"
-      @click.stop.prevent="postOrder()"
-      :disabled="isProcessing"
-    >結帳</button>
+      <button
+        class="btn btn-outline-secondary btn-block mt-3"
+        @click.stop.prevent="postOrder()"
+        :disabled="isProcessing"
+      >結帳</button>
+    </div>
   </div>
 </template>
 <script>
@@ -143,7 +154,7 @@ export default {
     },
     totalPrice() {
       let total = this.items.reduce((t, p) => t + p.sell_price * p.quantity, 0);
-      if (total * 0.3 <= this.coupon.discount_amount) {
+      if (total * 0.3 <= this.coupon.discount_amount && this.items.length > 0) {
         Toast.fire({
           type: "error",
           title: "折扣碼已未達金額限制"
