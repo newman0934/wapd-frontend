6ca5bfbdf73b5dbf05bdda5b514bab2e12c272ac
Vivian Lo <yunyubee@gmail.com>
2020-01-13 00:39:01 +0800

    remove console from product detail

--- src/components/productDetail.vue
+++ src/components/productDetail.vue
@@ -146,7 +146,6 @@ export default {
     currentURL() {
       const baseURL = "https://newman0934.github.io/wapd-frontend/#";
       let path = this.$route.path;
-      console.log(baseURL + path);
       return baseURL + path;
       // return "https://www.chickimmiu.com/products/t1901132";
     }
